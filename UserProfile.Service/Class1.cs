275da0ae9c37c722ffb58274ec15fe1c2025d2c0
Mohit <mohitjaiswal.bltr@gmail.com>
2025-11-15 22:33:38 +0530

    Revert "cleanup"
    This reverts commit 9c5b75c77ed6e640c6aad0733ff419b2cf54aa8f.

---
+++ UserProfile.Service/Class1.cs
@@ -0,0 +1,11 @@
+<<<<<<<< HEAD:Authentication.Data/Class1.cs
+﻿namespace Authentication.Data
+========
+﻿namespace UserProfile.Service
+>>>>>>>> origin/master:UserProfile.Service/Class1.cs
+{
+    public class Class1
+    {
+
+    }
+}
